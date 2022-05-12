1fb31d0b8c34678dfba6f7f5c9f979e4487730f4
Enrique Marina <eherraiz@myuax.com>
2022-05-12 09:33:38 +0200

    seguimos

--- examen.py
+++ examen.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import re
 
-df = pd.read_csv('navegacion.csv')
-print(df.head())
+# Lectura
+navegacion = pd.read_csv('navegacion.csv', sep = ';')
+conversiones = pd.read_csv('conversiones.csv', sep = ';')
+
+
+df_url = navegacion.loc[:, 'url_landing']
+df_url.to_csv('url_landing.csv', index = False)
+df_url = pd.read_csv('url_landing.csv', sep = '&')
+
+print(df_url.iloc[1][:])
+
+df_url.to_csv('prueba.csv')
+print(df_url.head())
